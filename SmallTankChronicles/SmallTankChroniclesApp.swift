6c87b8f06d6b0a5201facfe725204b2463d1405d
Sergey Chelak <sergey.chelak@gmail.com>
2025-01-13 15:26:16 +0200

    initial commit

---
+++ SmallTankChronicles/SmallTankChroniclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmallTankChroniclesApp.swift
+//  SmallTankChronicles
+//
+//  Created by Sergey on 13.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct SmallTankChroniclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
